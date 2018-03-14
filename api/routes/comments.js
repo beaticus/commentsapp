79ea9a4ca89f95cd65633535a44e02a2d92f99bb
Bea Caballero <beacaballero@Beas-MacBook-Air.local>
2018-03-14 15:51:47 +0000

    added routes

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,2 +1,44 @@
 const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /comments'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling POST requests to /comments'
+    });
+});
+
+router.get('/:commentId', (req, res, next) => {
+    const id = req.params.commentId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:commentId', (req, res, next) => {
+        res.status(200).json({
+        message: 'Updated comment!',
+            
+        });
+});
+
+router.delete('/:commentId', (req, res, next) => {
+    res.status(200).json({
+    message: 'Deleted comment!',
+        
+    });
+});
+
+module.exports = router;
